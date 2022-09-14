895f39e089023b1337607654484cf3f63e134dcf
Hieunvph14434 <nguyenvanhieu2001gl@gmail.com>
2022-09-14 23:38:55 +0700

    update api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { CustomerController } from './customer/customer.controller';
+import { CustomerModel } from './customer/customer.model';
 import express from 'express';
 import cors from 'cors';
 import { ReadConfig } from './config';
 import { MongoCommon } from './lib/mongodb';
+import { NewCustomerAPI } from './customer/customer.api';
 
 export async function main() {
     const config = await ReadConfig();
@@ -10,15 +13,18 @@ export async function main() {
     console.log('Connected to database');
 
     const database = client.db(config.database.db_name);
+
+    const customerModel = new CustomerModel(database);
+    await customerModel.init();
+    const customerController = new CustomerController(customerModel);
+    await customerController.init();
     const app = express();
     const PORT = process.env.PORT || 5000;
     app.use(express.json());
     app.use(cors());
     app.disable("x-powered-by");
 
-    app.get('/', (req, res)=>{
-        res.send('a');
-    });
+    app.use('api/customer', NewCustomerAPI(customerController));
 
     app.listen(PORT, ()=>{
         console.log('Server running!');
